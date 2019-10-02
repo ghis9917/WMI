76e1b91742b8fabd7166412ae2613411b10349a2
Massimo Monacchi <massimomonacchi@libero.it>
2019-10-02 14:46:20 +0200

    Create new server with Express, edit index.html src file (.js, .css)

--- Server/server.js
+++ Server/server.js
@@ -1,70 +1,39 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mobile = require('is-mobile');
-var mime = require('mime-types')
-const request = require('request');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 
-function invia(res,type,file){
-  var s = fs.createReadStream(file);
-  s.on('open', function () {
-      res.setHeader('Content-Type', type);
-      s.pipe(res);
-  });
-}
-
-function errore(res){
-  fs.readFile("../HTML/404.html", "UTF-8", function(err, html){
-    res.writeHead(404, {"Content-Type": "text/html"});
-    res.end(html);
-  });
-}
-
-http.createServer(function(req, res){
-    //rihieste GET
-    if( req.method == "GET"){
-      if(path.extname(req.url) != ""){
-        if(req.url == "/index.html"){
-            console.log("index")
-            invia(res,"text/html","../HTML/index.html")
-        }
-        else if(req.url != "/index.html"){
-          var myFile = (req.url).toString();
-          myFile = myFile.substring(myFile.indexOf('/')+1);
-          if(mime.lookup(myFile) == "text/html" ){
-            myFile = "../HTML/"+myFile;
-          }else{
-            myFile = "../"+myFile;
-          }
-          if (fs.existsSync(myFile)) {
-            invia(res,mime.lookup(myFile),myFile);
-          }else{
-              errore(res);
-          }
-        }
-      }
-      else{
-        if(req.url == "/"){
-            console.log("index")
-            invia(res,"text/html","../HTML/index.html")
-        }
-        //getValue?valore=.. api, ricerca su wikipedia, ritorna json
-        else if(req.url.match("getValue")){
-          var val  = req.url;
-          val = val.replace("/getValue?valore=", "");
-          getValue(val);
-        }
-        //getSound api, get Wikipedia description
-        else if(req.url.match("getWikipedia")){
-          var tools = require("./node_files/DBpedia.js");
-          tools.getDBPedia("Milano", "Place",res);
-        }
-        else if(req.url.match("getLuoghi")){
-        }
-      }
-  }
-    // Richiesta POST
-    else{
-    }
-}).listen(8000);  //porta 8000
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'../HTML/index.html'));
+});
+app.get('/index.html', (req, res) => {
+  res.sendFile(path.join(__dirname,'../HTML/index.html'));
+});
+app.get('/OpenLocationCode.js', (req, res) => {
+  res.contentType("text/javascript");
+  res.sendFile(path.join(__dirname,'../js/OpenLocationCode.js'));
+});
+app.get('/speechSynthesis.js', (req, res) => {
+  res.contentType("text/javascript");
+  res.sendFile(path.join(__dirname,'../js/speechSynthesis.js'));
+});
+app.get('/newMap.js', (req, res) => {
+  res.contentType("text/javascript");
+  res.sendFile(path.join(__dirname,'../js/newMap.js'));
+});
+app.get('/changeTheme.js', (req, res) => {
+  res.contentType("text/javascript");
+  res.sendFile(path.join(__dirname,'../js/changeTheme.js'));
+});
+app.get('/lrm-graphhopper.js', (req, res) => {
+  res.contentType("text/javascript");
+  res.sendFile(path.join(__dirname,'../js/lrm-graphhopper.js'));
+});
+app.get('/favicon.ico', (req, res) => {
+  console.log("jj")
+});
+app.get('/style.css', (req, res) => {
+    res.contentType("text/css");
+    res.sendFile(path.join(__dirname,'../css/style.css'));
+});
+app.listen(8000, () => console.log('Gator app listening on port 3000!'))
