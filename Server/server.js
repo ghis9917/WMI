8075e4db0551d5d66a5bec7dc923dc77a1e08ba5
Massimo Monacchi <massimomonacchi@libero.it>
2019-10-03 19:57:09 +0200

    Start Mongodb problem

--- Server/server.js
+++ Server/server.js
@@ -9,6 +9,34 @@ app.get('/', (req, res) => {
 app.get('/index.html', (req, res) => {
   res.sendFile(path.join(__dirname,'../HTML/index.html'));
 });
+
+app.get('/yt.html', (req, res) => {
+  const mongoose = require('mongoose');
+
+  var mongoURI = "mongodb://mongo_site181947:ASae0ahr@site181947.tw.cs.unibo.it:27017/myproject";
+      mongoose.connect(mongoURI, { useNewUrlParser: true , useUnifiedTopology: true});
+      mongoose.connection.once('open',()=>{
+          console.log('Connected to db');
+      });
+/*  var mongoURI = "mongodb://mongo_site181947:ASae0ahr@site181947.tw.cs.unibo.it:27017/myproject";
+  var MongoDB = mongoose.connect(mongoURI, { useNewUrlParser: true , useUnifiedTopology: true}).connection;
+  MongoDB.on('error', function(err) { console.log(err.message); });
+  MongoDB.once('open', function() {
+    console.log("mongodb connection open");
+  });/*
+   var MongoClient = require('mongodb').MongoClient;
+
+   var url = 'mongodb://site181947:ASae0ahr@mongo_site181947:27017/myproject';
+
+   MongoClient.connect(url, { useNewUrlParser: true , useUnifiedTopology: true  }, function(err, db) {
+     if(db == null){
+      res.send("non connesso")
+     }else{
+       res.send("non connesso");
+     }
+  });*/
+});
+
 app.get('/OpenLocationCode.js', (req, res) => {
   res.contentType("text/javascript");
   res.sendFile(path.join(__dirname,'../js/OpenLocationCode.js'));
