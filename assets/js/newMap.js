faec5f171c661fd636fead5d2c04aac0cacf93ec
franz9917 <ghis9917@gmail.com>
2019-11-15 20:27:36 +0100

    Modified popup setup

--- assets/js/newMap.js
+++ assets/js/newMap.js
@@ -84,9 +84,10 @@ function loadMarker() {
     $.when(getPOIs(q)).done(async function () {
       console.log(POIs);
       for (var key in POIs) {
-        var popup = "<h1 class=\"text-center\" style=\"margin: 1em;background-color: #000000;color: #ffffff\">" + key + "</h1>";
-        popup += "<img src=" + POIs[key].img + " ></img>";
-        popup += "<p class=\"text-center\" style=\"margin: 1em;background-color: #000000;color: #ffffff\">" + POIs[key].description.it + "</p>";
+        var popup = "<div id=\"popupContainer\" style=\"width: 300px;height: 250px;margin: 1em;padding: 1em;overflow: scroll;\"><div class=\"d-flex justify-content-between\"><div class=\"d-flex align-items-center\"><p>"+key+"</p></div><div><img style=\"width: 100px; height: 100px\" src="+POIs[key].img+"/></div></div><div style=\"padding-top: 1em;\"><p style=\"margin: 0px;\">"+POIs[key].description.it+"</p></div></div>";
+        //var popup = "<h1 class=\"text-center\" style=\"margin: 1em;background-color: #000000;color: #ffffff\">" + key + "</h1>";
+        //popup += "<img src=" + POIs[key].img + " ></img>";
+        //popup += "<p class=\"text-center\" style=\"margin: 1em;background-color: #000000;color: #ffffff\">" + POIs[key].description.it + "</p>";
         var m = L.marker([POIs[key].latitudeCenter, POIs[key].longitudeCenter], {
           bounceOnAdd: true,
           bounceOnAddOptions: { },
