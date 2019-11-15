095a6ee0bb112d40e546ae41867665b5509e7581
franz9917 <ghis9917@gmail.com>
2019-11-15 15:09:35 +0100

    update per robby

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const dps = require('dbpedia-sparql-client').default;
 const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
@@ -7,8 +8,11 @@ const path = require('path');
 const youtubeSearch = require('youtube-search');
 const f = require('./functions.js');
 var client = require('mongodb').MongoClient;
-const rickyKey = "AIzaSyBEpETjNZc18OP9L603YkzOvotslkQiBGI"
-const guiKey = "AIzaSyBFXSS4CBQKDc8yJtAdEruvXgAEHNwg8ko"
+var parseString = require('xml2js').parseString;
+
+const rickyKey = "AIzaSyBEpETjNZc18OP9L603YkzOvotslkQiBGI";
+const guiKey = "AIzaSyBFXSS4CBQKDc8yJtAdEruvXgAEHNwg8ko";
+const maxKey = "AIzaSyD5gNJnmZJlz4DsDcD1cFjgpqLfzX0LsFk";
 
 app.use(express.static('public')); // for serving the HTML file
 
@@ -28,54 +32,58 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
+app.get('/askDBPedia', (req, res) => {
+  var q = "select ?s1 as ?c1, (bif:search_excerpt (bif:vector ('MAGGIORE', 'PIAZZA'), ?o1)) as ?c2, ?sc, ?rank, ?g where {{{ select ?s1, (?sc * 3e-1) as ?sc, ?o1, (sql:rnk_scale (<LONG::IRI_RANK> (?s1))) as ?rank, ?g where  { quad map virtrdf:DefaultQuadMap { graph ?g { ?s1 ?s1textp ?o1 . ?o1 bif:contains  '(MAGGIORE AND PIAZZA)'  option (score ?sc)  . } } } order by desc (?sc * 3e-1 + sql:rnk_scale (<LONG::IRI_RANK> (?s1)))  limit 1  offset 0 }}}";
+  dps.client().query(q).timeout(15000).asJson().then((r) => {
+    res.send(r);
+  }).catch((e) => {
+    res.send(e);
+  });
+});
+
 app.get('/getDescription', (req, res) => {
   client.connect("mongodb://localhost:27017/",
     function (error, db) {
       if (!error) {
         var mydb = db.db("WMIdb");
-        mydb.collection("Descrizioni").find({ nome: req.query.val }).toArray(function (err, result) {
+        mydb.collection("Descrizioni").find({ nome: req.query.val }).toArray(async function (err, result) {
           if (err) throw err;
           if (result.length !== 0) {
-            res.send({val:result[0].descrizione});
+            res.send({ val: result[0].descrizione });
           } else {
-            //TODO DBPEDIA
-            res.send({val:"niente"});
+            var d = await f.get('http://localhost:8000/askDBPedia');
+            var e = await f.get(d.data.results.bindings[0].c1.value.replace("resource", "data")+".rdf");
+            parseString(e.data, function (err, result) {
+              var list = result["rdf:RDF"]["rdf:Description"][0]['rdfs:comment'];
+              var json = {};
+              for (var key in list) {
+                var chiave = list[key]["$"]["xml:lang"];
+                var valore = list[key]["_"];
+                json[chiave] = valore;
+              }
+              res.send({val : json});
+            });
           }
           db.close();
         });
       }
-      else{
+      else {
         console.log(error);
       }
     }
   );
 });
 
-function check(results){
-  for(var x in results){
-    for(var i in results){
-      var b = f.validator(results[x].description);
-      var d = f.validator(results[x].description);
-      if( x != i ){
-        if(d == b){
-          console.log(results[x].title);
-          console.log(results[i].title);
-        }
-      }
-    }
-  }
-  return results;
-}
-
 app.get('/getPOIs', (req, res) => {
   var opts = youtubeSearch.YouTubeSearchOptions = {
     maxResults: 50,
-    key: rickyKey
+    key: maxKey
   };
 
   youtubeSearch(req.query.searchQuery, opts, async (err, results) => {
     if (err) {
-      res.send({val : "niente"});
+      console.log(err);
+      res.send({ error: err.response.statusText });
     }
     else {
       console.log(results);
@@ -85,11 +93,11 @@ app.get('/getPOIs', (req, res) => {
       for (var key in results) {
         var item = results[key];
         if ((val = f.validator(item.description)) !== false) {
-          if (list.indexOf(val.plusCode) === -1){
+          if (list.indexOf(val.plusCode) === -1) {
             list.push(val.plusCode);
             POIs[item.title] = val.coords;
             POIs[item.title].videoId = item.id;
-            var d = await f.get('http://localhost:8000/getDescription?val='+item.title);
+            var d = await f.get('http://localhost:8000/getDescription?val=' + item.title);
             POIs[item.title].description = d.data.val;
           }
         }
