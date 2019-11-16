7fa5449a044d9bb28ce6223439d65cd7534eb53b
franz9917 <ghis9917@gmail.com>
2019-11-16 17:13:48 +0100

    Update

--- assets/js/newMap.js
+++ assets/js/newMap.js
@@ -3,62 +3,73 @@ var searchControl;
 var waypoints = [];
 var control = null;
 var distanceRouting = null;
-var routingMode = 'foot'
+var routingMode = "foot";
 var mymap;
-var POIs = {}
-var Desc = {}
+var POIs = {};
+var Desc = {};
 var dsts, ret;
 /*Sets up the map are of the html file
-*/
+ */
 var currentLocation;
 
-$(document).ready(function () {
+$(document).ready(function() {
   createMap();
   loadMarker();
 });
 
-$("#stop").click(function () {
-  document.getElementById('dad').hidden = true;
+$("#stop").click(function() {
+  document.getElementById("dad").hidden = true;
   try {
-    document.getElementById('iframe').stop();
-  }
-  catch{ }
+    document.getElementById("iframe").stop();
+  } catch {}
 });
 
 function createMap() {
-  var bounds = new L.LatLngBounds(new L.LatLng(-90, -180), new L.LatLng(90, 180));
-  mymap = L.map('mapid', {
+  var bounds = new L.LatLngBounds(
+    new L.LatLng(-90, -180),
+    new L.LatLng(90, 180)
+  );
+  mymap = L.map("mapid", {
     zoomControl: true,
     maxBounds: bounds,
     maxBoundsViscosity: 1.0
   });
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZnJhbno5OTE3IiwiYSI6ImNrMml5c2FmZDAwZ3YzaG5vODVvdmVxZXUifQ.Dm-BwkGmch8JSsJ9tTpo5w', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 3,
-    id: 'mapbox.streets'
-  }).addTo(mymap);
+  L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZnJhbno5OTE3IiwiYSI6ImNrMml5c2FmZDAwZ3YzaG5vODVvdmVxZXUifQ.Dm-BwkGmch8JSsJ9tTpo5w",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      minZoom: 3,
+      id: "mapbox.streets"
+    }
+  ).addTo(mymap);
 
-  mymap.on('locationerror', function (e) { });
-  mymap.setView([44.49394,11.3426944], 12);
-  navigator.geolocation.watchPosition(onLocationFound, onError, { enableHighAccuracy: true, maximumAge: 0 });
+  mymap.on("locationerror", function(e) {});
+  mymap.setView([44.49394, 11.3426944], 12);
+  navigator.geolocation.watchPosition(onLocationFound, onError, {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  });
 }
 
 function onLocationFound(position) {
-  var popup = "<p class=\"text-center\" style=\"margin: 1em;\">Sei qui!</p>";
+  var popup = '<p class="text-center" style="margin: 1em;">Sei qui!</p>';
   lat = position.coords.latitude;
   lon = position.coords.longitude;
-  try{
+  try {
     console.log("otherTimes");
     mymap.removeLayer(currentLocation);
   } catch {
     console.log("firstTime");
   }
-  currentLocation = L.marker([lat, lon]).bindPopup(popup).addTo(mymap);
+  currentLocation = L.marker([lat, lon])
+    .bindPopup(popup)
+    .addTo(mymap);
 }
 
-function onError(err){
+function onError(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
@@ -66,11 +77,10 @@ function getPOIs(q) {
   return $.ajax({
     type: "get",
     url: "/getPOIs?searchQuery=" + q,
-    success: function (data) {
+    success: function(data) {
       POIs = data;
     }
-  }
-  );
+  });
 }
 
 function sleep(ms) {
@@ -81,23 +91,34 @@ function loadMarker() {
   if (typeof lat !== "undefined") {
     var q = OpenLocationCode.encode(lat, lon, 4);
     q = q.replace("+", "");
-    $.when(getPOIs(q)).done(async function () {
+    $.when(getPOIs(q)).done(async function() {
       console.log(POIs);
       for (var key in POIs) {
-        var popup = "<div id=\"popupContainer\" style=\"width: 300px;height: 250px;margin: 1em;padding: 1em;overflow: scroll;\"><div class=\"d-flex justify-content-between\"><div class=\"d-flex align-items-center\"><p>"+key+"</p></div><div><img style=\"width: 100px; height: 100px\" src="+POIs[key].img+"/></div></div><div style=\"padding-top: 1em;\"><p style=\"margin: 0px;\">"+POIs[key].description.it+"</p></div></div>";
-        //var popup = "<h1 class=\"text-center\" style=\"margin: 1em;background-color: #000000;color: #ffffff\">" + key + "</h1>";
-        //popup += "<img src=" + POIs[key].img + " ></img>";
-        //popup += "<p class=\"text-center\" style=\"margin: 1em;background-color: #000000;color: #ffffff\">" + POIs[key].description.it + "</p>";
-        var m = L.marker([POIs[key].latitudeCenter, POIs[key].longitudeCenter], {
-          bounceOnAdd: true,
-          bounceOnAddOptions: { },
-          bounceOnAddCallback: function () { }
-        }).on('click', function(e){mymap.setView(m.getLatLng(), 12)}).bindPopup(popup).addTo(mymap);
+        var popup =
+          '<div id="popupContainer" style="width: 300px;height: 250px;padding: 1em;overflow: scroll;"><div class="d-flex justify-content-between"><div class="d-flex align-items-center"><p>' +
+          key +
+          '</p></div><div><img style="width: 100px; height: 100px" src=' +
+          POIs[key].img +
+          '/></div></div><div style="padding-top: 1em;"><p style="margin: 0px;">' +
+          POIs[key].description.it +
+          "</p></div></div>";
+        var m = L.marker(
+          [POIs[key].latitudeCenter, POIs[key].longitudeCenter],
+          {
+            bounceOnAdd: true,
+            bounceOnAddOptions: {},
+            bounceOnAddCallback: function() {}
+          }
+        )
+          .on("click", function(e) {
+            mymap.setView(m.getLatLng(), 12);
+          })
+          .bindPopup(popup)
+          .addTo(mymap);
         await sleep(250);
       }
     });
-  }
-  else {
+  } else {
     setTimeout(loadMarker, 1000);
   }
 }
--- server.js
+++ server.js
@@ -1,92 +1,124 @@
-const dps = require('dbpedia-sparql-client').default;
-const express = require('express');
-const fs = require('fs');
-const multer = require('multer');
-var toWav = require('audiobuffer-to-wav')
+const dps = require("dbpedia-sparql-client").default;
+const express = require("express");
+const fs = require("fs");
+const multer = require("multer");
+var toWav = require("audiobuffer-to-wav");
 const app = express();
-const path = require('path');
-const youtubeSearch = require('youtube-search');
-const f = require('./functions.js');
-var client = require('mongodb').MongoClient;
-var parseString = require('xml2js').parseString;
+const path = require("path");
+const youtubeSearch = require("youtube-search");
+const f = require("./functions.js");
+var client = require("mongodb").MongoClient;
+var parseString = require("xml2js").parseString;
 
 const rickyKey = "AIzaSyBEpETjNZc18OP9L603YkzOvotslkQiBGI";
 const guiKey = "AIzaSyBFXSS4CBQKDc8yJtAdEruvXgAEHNwg8ko";
+const rickykey_second = "AIzaSyB5PLURpl92Ix6gBHvgBMJ9s1JC7m69b2c";
 const maxKey = "AIzaSyD5gNJnmZJlz4DsDcD1cFjgpqLfzX0LsFk";
 
-app.use(express.static('public')); // for serving the HTML file
+app.use(express.static("public")); // for serving the HTML file
 
 const storage = multer.diskStorage({
-  destination: './public/uploads',
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
+  destination: "./public/uploads",
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
   }
-})
+});
 var upload = multer({
   destination: storage
 });
-var type = upload.single('upl');
+var type = upload.single("upl");
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-app.get('/askDBPedia', (req, res) => {
-  var q = "select ?s1 as ?c1, (bif:search_excerpt (bif:vector ('MAGGIORE', 'PIAZZA'), ?o1)) as ?c2, ?sc, ?rank, ?g where {{{ select ?s1, (?sc * 3e-1) as ?sc, ?o1, (sql:rnk_scale (<LONG::IRI_RANK> (?s1))) as ?rank, ?g where  { quad map virtrdf:DefaultQuadMap { graph ?g { ?s1 ?s1textp ?o1 . ?o1 bif:contains  '(MAGGIORE AND PIAZZA)'  option (score ?sc)  . } } } order by desc (?sc * 3e-1 + sql:rnk_scale (<LONG::IRI_RANK> (?s1)))  limit 1  offset 0 }}}";
-  dps.client().query(q).timeout(15000).asJson().then((r) => {
-    res.send(r);
-  }).catch((e) => {
-    res.send(e);
-  });
+app.get("/askDBPedia", (req, res) => {
+  var vector = req.query.name.split(" ");
+  for (string in vector) {
+    vector[string] = "'" + vector[string] + "'";
+    vector[string] = vector[string].toUpperCase();
+  }
+  var string = req.query.name.toUpperCase();
+  string = string.replace(/ /g, " AND ");
+  var q =
+    "select ?s1 as ?c1, (bif:search_excerpt (bif:vector (" +
+    vector +
+    " , 'BOLOGNA'), ?o1)) as ?c2, ?sc, ?rank, ?g where {{{ select ?s1, (?sc * 3e-1) as ?sc, ?o1, (sql:rnk_scale (<LONG::IRI_RANK> (?s1))) as ?rank, ?g where  { quad map virtrdf:DefaultQuadMap { graph ?g { ?s1 ?s1textp ?o1 . ?o1 bif:contains  '(" +
+    string +
+    " AND BOLOGNA)'  option (score ?sc)  . } } } order by desc (?sc * 3e-1 + sql:rnk_scale (<LONG::IRI_RANK> (?s1)))  limit 1  offset 0 }}}";
+  dps
+    .client()
+    .query(q)
+    .timeout(15000)
+    .asJson()
+    .then(r => {
+      res.send(r);
+    })
+    .catch(e => {
+      res.send(e);
+    });
 });
 
-app.get('/getDescription', (req, res) => {
-  client.connect("mongodb://localhost:27017/",
-    function (error, db) {
-      if (!error) {
-        var mydb = db.db("WMIdb");
-        mydb.collection("Descrizioni").find({ nome: req.query.val }).toArray(async function (err, result) {
+app.get("/getDescription", (req, res) => {
+  client.connect("mongodb://localhost:27017/", function(error, db) {
+    if (!error) {
+      var mydb = db.db("WMIdb");
+      mydb
+        .collection("Descrizioni")
+        .find({ nome: req.query.val })
+        .toArray(async function(err, result) {
           if (err) throw err;
           if (result.length !== 0) {
             res.send({ val: result[0].descrizione });
           } else {
-            var d = await f.get('http://localhost:8000/askDBPedia');
-            var e = await f.get(d.data.results.bindings[0].c1.value.replace("resource", "data")+".rdf");
-            parseString(e.data, function (err, result) {
-              var list = result["rdf:RDF"]["rdf:Description"][0]['rdfs:comment'];
-              var img = result["rdf:RDF"]["rdf:Description"][0]["dbo:thumbnail"][0]["$"]["rdf:resource"];
-              var json = {};
-              for (var key in list) {
-                var chiave = list[key]["$"]["xml:lang"];
-                var valore = list[key]["_"];
-                json[chiave] = valore;
-              }
-              res.send({val : json, img: img});
-            });
+            var d = await f.get(
+              "http://localhost:8000/askDBPedia?name=" + req.query.val
+            );
+            try {
+              var e = await f.get(
+                d.data.results.bindings[0].c1.value.replace(
+                  "resource",
+                  "data"
+                ) + ".rdf"
+              );
+              parseString(e.data, function(err, result) {
+                var json = {};
+                var list =
+                  result["rdf:RDF"]["rdf:Description"][0]["rdfs:comment"];
+                for (var key in list) {
+                  var chiave = list[key]["$"]["xml:lang"];
+                  var valore = list[key]["_"];
+                  json[chiave] = valore;
+                }
+                var img =
+                  result["rdf:RDF"]["rdf:Description"][0]["dbo:thumbnail"][0][
+                    "$"
+                  ]["rdf:resource"];
+                res.send({ val: json, img: img });
+              });
+            } catch (error) {
+              res.send({ val: "NF", img: "NF" });
+            }
           }
           db.close();
         });
-      }
-      else {
-        console.log(error);
-      }
+    } else {
+      console.log(error);
     }
-  );
+  });
 });
 
-app.get('/getPOIs', (req, res) => {
-  var opts = youtubeSearch.YouTubeSearchOptions = {
+app.get("/getPOIs", (req, res) => {
+  var opts = (youtubeSearch.YouTubeSearchOptions = {
     maxResults: 50,
-    key: guiKey
-  };
+    key: rickykey_second
+  });
 
   youtubeSearch(req.query.searchQuery, opts, async (err, results) => {
     if (err) {
       console.log(err);
       res.send({ error: err.response.statusText });
-    }
-    else {
+    } else {
       console.log(results);
       //results = check(results);
       var POIs = {};
@@ -98,7 +130,9 @@ app.get('/getPOIs', (req, res) => {
             list.push(val.plusCode);
             POIs[item.title] = val.coords;
             POIs[item.title].videoId = item.id;
-            var d = await f.get('http://localhost:8000/getDescription?val=' + item.title);
+            var d = await f.get(
+              "http://localhost:8000/getDescription?val=" + item.title
+            );
             POIs[item.title].description = d.data.val;
             POIs[item.title].img = d.data.img;
           }
@@ -109,45 +143,53 @@ app.get('/getPOIs', (req, res) => {
   });
 });
 
-app.get('/poi', (req, res) => {
-  res.sendFile(path.join(__dirname, './poi.json'));
+app.get("/poi", (req, res) => {
+  res.sendFile(path.join(__dirname, "./poi.json"));
 });
 
-
-app.get('/audio.wav', (req, res) => {
-  res.sendFile(path.join(__dirname, './audio.wav'));
+app.get("/audio.wav", (req, res) => {
+  res.sendFile(path.join(__dirname, "./audio.wav"));
 });
 
-app.get('/insertDescription', (req, res) => {
-  client.connect("mongodb://localhost:27017/",
-    function (error, db) {
-      if (!error) {
-        var mydb = db.db("WMIdb");
-        var myobj = { nome: req.query.nome, descrizione: req.query.des };
-        mydb.collection("Descrizioni").insertOne(myobj, function (err, res) {
-          if (err) throw err;
-          console.log("1 document inserted");
-          db.close();
-        });
-      }
+app.get("/insertDescription", (req, res) => {
+  client.connect("mongodb://localhost:27017/", function(error, db) {
+    if (!error) {
+      var mydb = db.db("WMIdb");
+      var myobj = { nome: req.query.nome, descrizione: req.query.des };
+      mydb.collection("Descrizioni").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+      });
     }
-  );
+  });
 });
 
-app.post('/api/test', type, function (req, res) {
+app.post("/api/test", type, function(req, res) {
   console.log(req.body);
   console.log(req.file);
   var f = req.file;
   console.log(f.buffer);
-  fs.writeFile('sample.mp3', f.buffer, function (err) {
+  fs.writeFile("sample.mp3", f.buffer, function(err) {
     res.sendStatus(err ? 500 : 200);
   });
 });
 
-app.get('*', (req, res) => {
+app.get("*", (req, res) => {
   var ext = path.extname(req.url);
-  if (ext === ".css" || ext === ".html" || ext === ".js" || ext === ".jpg" || ext === ".png" || ext === ".woff" || ext === ".woff2" || ext === ".ttf" || ext === ".svg" || ext === ".eot") {
-    res.sendFile(path.join(__dirname, './' + req.url));
+  if (
+    ext === ".css" ||
+    ext === ".html" ||
+    ext === ".js" ||
+    ext === ".jpg" ||
+    ext === ".png" ||
+    ext === ".woff" ||
+    ext === ".woff2" ||
+    ext === ".ttf" ||
+    ext === ".svg" ||
+    ext === ".eot"
+  ) {
+    res.sendFile(path.join(__dirname, "./" + req.url));
   } else if (ext === ".ico") {
     res.status(204).json({ nope: true });
   } else {
@@ -155,4 +197,4 @@ app.get('*', (req, res) => {
   }
 });
 
-app.listen(8000, () => console.log('Gator app listening on port 8000!'))
+app.listen(8000, () => console.log("Gator app listening on port 8000!"));
