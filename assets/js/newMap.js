dcfb5a9bfd401b7af63965d0d8f00bac8ee9bf39
riccardo preite <riccardo.preite.10@gmail.com>
2019-11-29 15:01:40 +0100

    Fixed distance

--- assets/js/newMap.js
+++ assets/js/newMap.js
@@ -77,7 +77,6 @@ return new Promise((resolve,reject) => {
 }
 
 function onLocationFound(position) {
-  console.log("LOCATION FOUND")
   var popup = "<p class=\"text-center\" style=\"margin: 1em;\">Sei qui!</p>";
   lat = position.coords.latitude;
   lon = position.coords.longitude;
@@ -181,7 +180,6 @@ function addRouting() {
 function checkDistance(distance,instruction){
   if(distance <= 20){
     //Time to talk description of POI
-    console.log(referenceTable)
     var la = POIs[referenceTable[minIndexes[currentDestination]]].latitudeCenter;
     var lo = POIs[referenceTable[minIndexes[currentDestination]]].longitudeCenter;
     var mark = new L.marker([la, lo], {
@@ -200,7 +198,6 @@ function getPOIs(q) {
     url: "/getPOIs?searchQuery=" + q,
     success: function(data) {
       POIs = data;
-      console.log(POIs)
     }
   });
 }
@@ -242,7 +239,6 @@ function createPlayer() {
   var timer = null;
   if (Object.keys(POIs).length !== 0) {
     clearTimeout(timer);
-    console.log(mymap.getSize())
     addRouting();
     elaborateDistance();
   } else {
@@ -254,7 +250,7 @@ function addPlayButton() {
   L.easyButton({
     states: [
       {
-        stateName: "null", // name the state
+        stateName: "search", // name the state
         icon: "fas fa-bong", // and define its properties
         title: "Click to get directions to the nearest POI", // like its title
         onClick: function(btn) {
@@ -279,121 +275,43 @@ function populatePopup() {
         ? POIs[referenceTable[minIndexes[currentDestination]]].img
         : "https://cdn.shopify.com/s/files/1/1552/7487/products/good_game_gg-smiley_fornite_spray_emote_printed_sticker_grfxp_grafixpressions_720x.jpg?v=1525975056"
     );
-
   }
   $("#popupImg").attr("style", "width: 50%;height: auto; float: right;");
   $("#popupContainer").css("width", "calc(100% - 2em)");
 }
 
 function elaborateDistance() {
+
   var count = 0;
-  var url =
-    "https://maps.googleapis.com/maps/api/distancematrix/xml?origins=" + lat + "," + lon + "|";
-  var point = "";
-  // var url2 = url;
-    console.log(referenceTable)
-  for (var i in POIs) {
+  calculateDistance(lat,lon,0);
+  for (var i in POIs){
     referenceTable[count] = i;
     count++;
-    point +=
-        POIs[i].latitudeCenter + "," + POIs[i].longitudeCenter;
-    if(count <=  12){
-      point += "|";
-    }
+    calculateDistance(POIs[i].latitudeCenter, POIs[i].longitudeCenter,count);
+  }
+  count = -1;
+  do{
+     count = createRoute(count+1);
+  }while(count != -1);
+  currentDestination = 0;
+  routingTo(POIs[referenceTable[minIndexes[currentDestination]]]);
+  populatePopup();
   }
 
-  url += point;
-  url +=
-    "&destinations=" + point + "&key=AIzaSyCAXQP_4KlAztXqWzAOvjv7Pa7DWIUb42U";
-
-  console.log(url)
-    count = 0 ;
-    for (var i in POIs) {
-      if(count > 10 ){
-      referenceTable[count] = i;
-      url2 +=
-        "&to_point=" + POIs[i].latitudeCenter + "," + POIs[i].longitudeCenter;
-      }
-      count++;
-    }
-    url2 +=
-      "&type=json&vehicle=foot&debug=true&out_array=distances&key=653995f0-72fe-4af8-b598-60e50479a0c2";
-      $.when(getDistance(url)).done(async function() {
-        // $.when(getDistance(url2)).done(async function() {
-          console.log(DSTs)
-              var index = 0;
-              do{
-                index = createRoute(index);
-              }
-              while(index != -1);
-             currentDestination = 0;
-             routingTo(POIs[referenceTable[minIndexes[currentDestination]]]);
-             populatePopup();
-           // });
-   });
-
-//   var count = 0;
-//   calcultateDistance(lat,lon,0);
-//   for (var i in POIs){
-//     referenceTable[count] = i;
-//
-//     calcultateDistance(POIs[i].latitudeCenter, POIs[i].longitudeCenter,count);
-//     count++;
-//   }
-//   console.log(DSTs);
-//
-//   count = 0;
-//   do{
-//      count = createRoute(count);
-//      console.log(count)
-//   }while(count != -1);
-// // console.log("MIN INDEXES")
-// // console.log(minIndexes)
-//   currentDestination = 0;
-//   routingTo(POIs[referenceTable[minIndexes[currentDestination]]]);
-//   populatePopup();
-  }
-
-  function calcultateDistance(slat,slon,index){
-    var count = 0;
+  function calculateDistance(slat,slon,index){
+    var x = L.latLng(slat,slon);
     DSTs[index] = {};
     for (var i in POIs) {
-      if(POIs[i].latitudeCenter != slat && POIs[i].longitudeCenter != slon){
-        var dist = calcCrow(slat,slon,POIs[i].latitudeCenter, POIs[i].longitudeCenter);
+        var dist = x.distanceTo(L.latLng(POIs[i].latitudeCenter,POIs[i].longitudeCenter))
         DSTs[index][i] = {};
-        DSTs[index][i] = dist * 1000;
-        count++;
-      }
+        DSTs[index][i] = dist ;
     }
   }
-
-
-  function calcCrow(lat1, lon1, lat2, lon2)
-    {
-      var R = 6371; // km
-      var dLat = toRad(lat2-lat1);
-      var dLon = toRad(lon2-lon1);
-      var lat1 = toRad(lat1);
-      var lat2 = toRad(lat2);
-
-      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-      var d = R * c;
-      return d;
-    }
-
-    // Converts numeric degrees to radians
-    function toRad(Value)
-    {
-        return Value * Math.PI / 180;
-    }
 
   function createRoute(i) {
     var min = 40075000;
     var minIndex = -1;
     var count = 0;
-    console.log( DSTs[i])
     for (var index in DSTs[i]) {
       if (
         DSTs[i][index] > 0 &&
@@ -402,15 +320,11 @@ function elaborateDistance() {
       ) {
           minIndex = count;
           min = DSTs[i][index];
-          // console.log("AG  GIORNATO MIN " +  DSTs[i][index] )
-          // console.log(m  inIndex)
-
       }
       count++;
     }
     if(minIndex != -1){
       minIndexes.push(Number(minIndex));
-      console.log("pushing " + minIndex)
     }
     return minIndex;
   }
@@ -421,16 +335,6 @@ function getDistance(q) {
     success: function(data) {
       console.lof
       DSTs = data;
-      // console.log(data)
-      // if(jQuery.isEmptyObject(DSTs)) {
-      //   console.log("PRIMO CASO VUOTO")
-      //   DSTs = data;
-      // }
-      // else {
-      //   for(item in data){
-      //     DSTs[item] = data[item];
-      //   }
-      // }
     }
   });
 }
@@ -454,8 +358,6 @@ $("#prev").on("click", function() {
 
 $("#next").on("click", function() {
   if(currentDestination < minIndexes.length -   1){
-    console.log(referenceTable[minIndexes[currentDestination]])
-    console.log(minIndexes[currentDestination])
     currentDestination++;
     routingTo(POIs[referenceTable[minIndexes[currentDestination]]]);
     populatePopup();
